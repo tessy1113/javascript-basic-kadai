88ee9a46630da6f2968e6d2a7cc2c163c296da62
tessy1113 <tessy1113@gmail.com>
2025-01-05 14:14:38 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,15 +1,14 @@
 //変数の宣言
 let weight;
 let height;
+let bmi;
 
 //値の代入
-weight = '68'
-height = '1.7'
+weight = 68
+height = 1.7
+bmi = weight/(height*height)
 
 // コンソールへの出力
 console.log(weight);
 console.log(height);
-
-//四則演算
-console.log(weight/(height*height))
-
+console.log(bmi);

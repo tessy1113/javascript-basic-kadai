4f507a526132a5946b2d092cd09c7d734b8fb1c6
tessy1113 <tessy1113@gmail.com>
2025-01-05 12:54:34 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+//変数の宣言
+let weight;
+let height;
+
+//値の代入
+weight = '68'
+height = '1.7'
+
+// コンソールへの出力
+console.log(weight);
+console.log(height);
+
+//四則演算
+console.log(weight/(height*height))
+
